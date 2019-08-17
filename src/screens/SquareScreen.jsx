005ef024d1908c5943b3a64567b01deae5cb8069
Renato Oliveira <renatorops@gmail.com>
2019-08-17 12:52:54 -0300

    Reusable State Update. Aula - 50

--- src/screens/SquareScreen.jsx
+++ src/screens/SquareScreen.jsx
@@ -6,22 +6,54 @@ const SquareScreen = () => {
 	const [red, setRed] = useState(0);
 	const [green, setGreen] = useState(0);
 	const [blue, setBlue] = useState(0);
+
+	const COLOR_INCREMENT = 15;
+
+	const setColor = (color, change) => {
+		// color 'red', 'green', 'blue'
+		// change +15, -15
+		switch (color) {
+			case 'red':
+				red + change > 255 || red + change < 0 ? null : setRed(red + change);
+				return;
+			case 'green':
+				green + change > 255 || green + change < 0
+					? null
+					: setGreen(green + change);
+				return;
+			case 'blue':
+				blue + change > 255 || blue + change < 0
+					? null
+					: setBlue(blue + change);
+				return;
+			default:
+				return;
+		}
+	};
+
 	return (
 		<View>
 			<ColorCounter
-				onIncrease={() => setRed(red + 1)}
-				onDecrease={() => setRed(red - 1)}
+				onIncrease={() => setColor('red', COLOR_INCREMENT)}
+				onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
 				color="Red"
 			/>
 			<ColorCounter
-				onIncrease={() => setBlue(blue + 1)}
-				onDecrease={() => setBlue(blue - 1)}
-				color="Blue"
+				onIncrease={() => setColor('green', COLOR_INCREMENT)}
+				onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
+				color="Green"
 			/>
 			<ColorCounter
-				onIncrease={() => setGreen(green + 1)}
-				onDecrease={() => setGreen(green - 1)}
-				color="Green"
+				onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+				onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
+				color="Blue"
+			/>
+			<View
+				style={{
+					height: 150,
+					width: 150,
+					backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+				}}
 			/>
 		</View>
 	);
