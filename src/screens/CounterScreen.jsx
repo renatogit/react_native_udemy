443132478c998e149136ac22e013fe8d822292b5
Renato Oliveira <renatorops@gmail.com>
2019-08-18 12:43:25 -0300

    Exercise Solution. Aula - 57.(CounterScreen.jsx)

--- src/screens/CounterScreen.jsx
+++ src/screens/CounterScreen.jsx
@@ -1,21 +1,33 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { Text, View, Button } from 'react-native';
 
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'handle_increment':
+			return { ...state, counter: state.counter + action.payload };
+		case 'handle_decrement':
+			return { ...state, counter: state.counter - action.payload };
+		default:
+			return;
+	}
+};
+
 const CounterDetail = () => {
-	const [counter, setCounter] = useState(0);
+	const [state, dispatch] = useReducer(reducer, { counter: 0 });
+	const { counter } = state;
 
 	return (
 		<View>
 			<Button
 				title="Increment"
 				onPress={() => {
-					setCounter(counter + 1);
+					dispatch({ type: 'handle_increment', payload: 1 });
 				}}
 			/>
 			<Button
 				title="Decrement"
 				onPress={() => {
-					setCounter(counter - 1);
+					dispatch({ type: 'handle_decrement', payload: 1 });
 				}}
 			/>
 			<Text>Current Count: {counter}</Text>
